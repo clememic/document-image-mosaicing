d52df7d90ad1d2aae36f40b77919e5feb890a74b
Michael Clement <michael.clement@mikya.me>
2014-01-18 19:29:22 +0100

    Replace ORB features with SIFT features

--- src/Registration.cpp
+++ src/Registration.cpp
@@ -8,22 +8,24 @@ vector<ImageFeatures> Registration::getSurfFeatures(const vector<Mat>& imgs, dou
 		int num_octaves, int num_layers, int num_octaves_descr, int num_layers_descr) {
 	Ptr<FeaturesFinder> surf = new SurfFeaturesFinder(hess_thresh, num_octaves, num_layers,
 		num_octaves_descr, num_layers_descr);
-	return getFeatures(imgs, surf);
-}
-
-vector<ImageFeatures> Registration::getOrbFeatures(const vector<Mat>& imgs, Size grid_size,
-		int nfeatures, float scaleFactor, int nlevels) {
-	Ptr<FeaturesFinder> orb = new OrbFeaturesFinder(grid_size, nfeatures, scaleFactor, nlevels);
-	return getFeatures(imgs, orb);
-}
-
-vector<ImageFeatures> Registration::getFeatures(const vector<Mat>& imgs, FeaturesFinder* features_finder) {
 	vector<ImageFeatures> features(imgs.size());
 	for (unsigned int i = 0; i < imgs.size(); i++) {
-		(*features_finder)(imgs[i], features[i]);
+		(*surf)(imgs[i], features[i]);
 		features[i].img_idx = i;
 	}
-	features_finder->collectGarbage();
+	surf->collectGarbage();
+	return features;
+}
+
+vector<ImageFeatures> Registration::getSiftFeatures(const vector<Mat>& imgs, int nfeatures,
+		int nOctaveLayers, double contrastThreshold, double edgeThreshold, double sigma) {
+	Ptr<SIFT> sift = new SIFT(nfeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma);
+	vector<ImageFeatures> features(imgs.size());
+	for (unsigned int i = 0; i < imgs.size(); i++) {
+		sift->detect(imgs[i], features[i].keypoints);
+		sift->compute(imgs[i], features[i].keypoints, features[i].descriptors);
+		features[i].img_idx = i;
+	}
 	return features;
 }
 
--- src/Registration.hpp
+++ src/Registration.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <opencv2/stitching/stitcher.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 
 class Registration {
 
@@ -11,11 +12,11 @@ public:
 	static std::vector<cv::detail::ImageFeatures> getSurfFeatures(const std::vector<cv::Mat>& imgs,
 		double hess_thresh=300, int num_octaves=3, int num_layers=4, int num_octaves_descr=3, int num_layers_descr=4);
 
-	static std::vector<cv::detail::ImageFeatures> getOrbFeatures(const std::vector<cv::Mat>& imgs,
-		cv::Size grid_size=cv::Size(3,1), int nfeatures=1500, float scaleFactor=1.3f, int nlevels=5);
+	static std::vector<cv::detail::ImageFeatures> getSiftFeatures(const std::vector<cv::Mat>& imgs,
+		int nfeatures=0, int nOctaveLayers=3, double contrastThreshold=0.04, double edgeThreshold=10, double sigma=1.6);
 
 	static std::vector<cv::detail::MatchesInfo> getMatches(const std::vector<cv::detail::ImageFeatures> &features,
-		float match_conf=0.3f, int num_matches_thresh1=6, int num_matches_thresh2=6);
+		float match_conf=0.65f, int num_matches_thresh1=6, int num_matches_thresh2=6);
 
 	static std::vector<cv::detail::CameraParams> estimateHomographies(const std::vector<cv::detail::ImageFeatures>& features,
 		const std::vector<cv::detail::MatchesInfo>& pairwise_matches);
@@ -29,9 +30,6 @@ public:
 		double conf_thresh=1.);
 
 private:
-
-	static std::vector<cv::detail::ImageFeatures> getFeatures(const std::vector<cv::Mat>& imgs,
-		cv::detail::FeaturesFinder* features_finder);
 
 	static void bundleAdjuster(const std::vector<cv::detail::ImageFeatures>& features,
 		const std::vector<cv::detail::MatchesInfo>& pairwise_matches, std::vector<cv::detail::CameraParams>& cameras,
